8856981f98e697c6fcc0aa572a18a0101d53a8fe
inna-i <iivashchuk@talend.com>
2020-05-14 17:39:12 +0300

    fix: userInfo, move styles to right folder

--- src/App.js
+++ src/App.js
@@ -1,38 +1,30 @@
 import React from 'react';
 import Main from './components/Main/index';
 
-import './App.css';
 
 class App extends React.Component {
-		constructor(props) {
-				super(props);
-				this.state = {
-					user: {},
-					isLoaded: false,
-				};
-		}
+	constructor(props) {
+		super(props);
+		this.state = {
+			user: null,
+		};
+	}
 
-		getUser(userId) {
-			fetch(`https://api.github.com/users`)
-				.then(response => response.json())
-				.then(result => result.find(user => user.id === userId))
-				.then(user => this.setState({
-					user: user,
-					isLoaded: true
-				}))
-				.catch(reason => console.log('Error: ' + reason));
-		}
-
-		componentDidMount() {
-			// TODO: retrieve id from url.
-			this.getUser(5);
-		}
+	componentDidMount() {
+		// fetch account by userName
+		fetch(`https://api.github.com/users/inna-i`)
+			.then(resp => resp.json())
+			.then(user => this.setState({
+				user: user,
+			}))
+			.catch(reason => console.log('Error: ' + reason));
+	}
 
 	render() {
-			const {user, isLoaded} = this.state;
+		if (!this.state.user) return (<span>Loading...</span>);
 
 		return (<Main user={this.state.user} />);
-		}
+	}
 }
 
 export default App;
