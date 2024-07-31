43058770be7891c23210f55b6954cb78f9b48b4a
inna-i <iivashchuk@talend.com>
2024-07-31 08:55:45 +0200

    feat: update header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,46 +1,52 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell, faInfo } from "@fortawesome/free-solid-svg-icons";
 
-import { GITHUB_API } from '../../config/constants';
-import { UserContext } from '../../Context/UserContext';
-import UserInfo from '../UserInfo';
-import './style.css';
+import { GITHUB_API } from "../../config/constants";
+import { UserContext } from "../../Context/UserContext";
+import UserInfo from "../UserInfo";
+import "./style.css";
 
 function Header() {
-    const [events, setEvents] = useState(0);
- 
-    useEffect(() => {
-        fetch(`${GITHUB_API}/inna-i/received_events`)
-            .then(res => res.json())
-            .then(res => setEvents(res.length))
-            .catch(e => console.error(e))
-    }, [events])
+  const [events, setEvents] = useState(0);
+  const [show, setShow] = useState(false);
 
-   
-    return(
-        <header>
+  useEffect(() => {
+    fetch(`${GITHUB_API}/inna-i/received_events`)
+      .then((res) => res.json())
+      .then((res) => setEvents(res.length))
+      .catch((e) => console.error(e));
+  }, [events]);
+
+  return (
+    <UserContext.Consumer>
+      {({ user }) =>
+        user && (
+          <header>
             <div className="info-block">
-                {/* <UserContext.Consumer>
-                
-                    {({ user }) => user && (
-                        <ul>
-                            <li>Recieved events: {events}</li>
-                            <li>Followers: {user.followers}</li>
-                            <li>Repos: {user.public_repos}</li>
-                        </ul>
-                    )}
-                </UserContext.Consumer>                */}
+              <button onClick={() => window.open('https://github.com/inna-i')}>
+                <FontAwesomeIcon icon={faInfo} />
+              </button> 
+              
+              <button onClick={() => setShow(!show)}>
+                <FontAwesomeIcon icon={faBell} />
+              </button>
+
+              {show && (
+                <ul>
+                  <li>Recieved events ({events})</li>
+                  <li>Followers ({user.followers})</li>
+                  <li>Repos ({user.public_repos})</li>
+                </ul>
+              )}
             </div>
-            <UserContext.Consumer>
-                {
-                    ({ user }) => user && (
-                        <UserInfo
-                            login={user.login}
-                            avatar={user.avatar_url} />
-                    )
-                }
-            </UserContext.Consumer>
-        </header>
-    );
+
+            <UserInfo login={user.login} avatar={user.avatar_url} />
+          </header>
+        )
+      }
+    </UserContext.Consumer>
+  );
 }
 
 export default Header;
